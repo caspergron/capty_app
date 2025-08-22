79dcaabdd55d7c73466c060485dfca92c4f460e0
md.tanviranwarrafi <tanviranwarrafi@gmail.com>
2025-08-22 12:35:05 +0600

    update: grid path design done

---
+++ lib/helpers/graph_helper.dart
@@ -0,0 +1,29 @@
+import 'package:fl_chart/fl_chart.dart';
+
+import 'package:app/extensions/number_ext.dart';
+import 'package:app/models/disc/user_disc.dart';
+
+var _CENTER = 5.0;
+var _INITIAL_SPOT = FlSpot(_CENTER, 0);
+
+class GraphHelper {
+  List<ScatterSpot> getLineBarSpots(List<UserDisc> discList) {
+    if (discList.isEmpty) return [];
+    List<ScatterSpot> scatterSpots = [];
+    discList.forEach((item) => scatterSpots.add(ScatterSpot(item.turn_plus_fade, item.speed.nullToDouble)));
+    return scatterSpots;
+  }
+
+  Map<String, double> getMaxValuesForGrid(List<ScatterSpot> scatterSpots) {
+    var maxX = scatterSpots.reduce((item1, item2) => item1.x > item2.x ? item1 : item2).x;
+    var maxY = scatterSpots.reduce((item1, item2) => item1.y > item2.y ? item1 : item2).y;
+    return {'max_x': maxX, 'max_y': maxY};
+  }
+
+  List<ScatterSpot> getScatterSpots(List<UserDisc> discList) {
+    if (discList.isEmpty) return [];
+    List<ScatterSpot> scatterSpots = [];
+    discList.forEach((item) => scatterSpots.add(ScatterSpot(item.turn_plus_fade, item.speed.nullToDouble)));
+    return scatterSpots;
+  }
+}
---
+++ lib/models/chart/graph_model.dart
@@ -0,0 +1,21 @@
+import 'package:fl_chart/fl_chart.dart';
+
+import 'package:app/models/chart/line_bar.dart';
+
+class GraphModel {
+  double maxX;
+  double maxY;
+  List<LineBar> lineBars;
+  List<ScatterSpot> graphSpots;
+
+  GraphModel({this.maxX = 0, this.maxY = 0, this.graphSpots = const [], this.lineBars = const []});
+
+  Map<String, dynamic> toJson() {
+    var map = <String, dynamic>{};
+    map['max_x'] = maxX;
+    map['max_y'] = maxY;
+    map['graph_spots'] = graphSpots;
+    map['line_bars'] = lineBars;
+    return map;
+  }
+}
